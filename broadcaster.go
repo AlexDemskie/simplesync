51877d3f6be76c0f550e7f338d0115c565d04225
AlexD <alex.demskie@gmail.com>
2018-03-25 23:39:37 -0700

    removing unnecessary locks

--- broadcaster.go
+++ broadcaster.go
@@ -1,19 +1,12 @@
 package simplesync
 
-import (
-	"sync"
-	"sync/atomic"
-)
+import "sync"
 
 // Broadcaster is a flexible many-to-many lock to be used in place of a waitGroup
 type Broadcaster struct {
 	noCopy    noCopy
 	sending   *sync.Mutex
-	startOnce *sync.Once
 	startChan chan struct{}
-	received  int32
-	doneOnce  *sync.Once
-	doneChan  chan struct{}
 }
 
 // Len gets the number of receivers
@@ -32,25 +25,16 @@ func NewBroadcaster(receiverCount int) (b *Broadcaster) {
 	}
 	return &Broadcaster{
 		sending:   &sync.Mutex{},
-		startOnce: &sync.Once{},
 		startChan: make(chan struct{}, receiverCount),
-		received:  0,
-		doneOnce:  &sync.Once{},
-		doneChan:  make(chan struct{}, 1),
 	}
 }
 
 // Send unblocks the goroutines and waits for their completion
 func (b *Broadcaster) Send() int {
 	b.sending.Lock()
-	b.startOnce.Do(func() {
-		b.doneOnce = &sync.Once{}
-		for i := 0; i < cap(b.startChan); i++ {
-			b.startChan <- struct{}{}
-		}
-		<-b.doneChan
-		b.doneChan = make(chan struct{}, 1)
-	})
+	for i := 0; i < cap(b.startChan); i++ {
+		b.startChan <- struct{}{}
+	}
 	b.sending.Unlock()
 	return cap(b.startChan)
 }
@@ -58,13 +42,6 @@ func (b *Broadcaster) Send() int {
 // Receive is called by the goroutines waiting on the Broadcast
 func (b *Broadcaster) Receive() {
 	<-b.startChan
-	if atomic.AddInt32(&b.received, 1) >= int32(cap(b.startChan)) {
-		b.doneOnce.Do(func() {
-			atomic.AddInt32(&b.received, -int32(cap(b.startChan)))
-			b.startOnce = &sync.Once{}
-			close(b.doneChan)
-		})
-	}
 	return
 }
 
